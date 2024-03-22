9eb0adb5e8998765e3e781d12fb2702417e7e7c3
NerdboyQ <princtonb@gmail.com>
2024-03-22 12:41:45 -0700

    working on bt device element and scanning functionality

--- BT_Lights/src/webapp/flask/bt_iot_webapp/static/js/custom_elements.js
+++ BT_Lights/src/webapp/flask/bt_iot_webapp/static/js/custom_elements.js
@@ -1,22 +1,59 @@
-class BluetoothDevBox extends HTMLElement {
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        /* @import url();  for importing custom css*/
+        div {
+            border-radius: 5px;
+            corner-radius: 15px;
+        }
+        :host {
+            /* for the shadow root */
+            background-color: #57A0D3;
+            border-radius: 5px;
+            display: block;
+            margin: 20px;
+            padding: 20px;
+        }
+        /* conditional formatting based on the element type */
+        :host() {
+
+        }
+        /* conditional style based on parent element*/
+        :host-context() {
+
+        }
+        /* anything that's slotted into the element */
+        /* NOTE: This is loaded BEFORE the main css is loaded, so
+           so the changes applied to slotted elements will be overwritten.
+           to circumvent this, we use the '!important' key to ensure the
+           styling is maintained for this custom element.*/
+        ::slotted(*){
+            color: #7EF9FF !important;
+        }
+    </style>
+    <div>
+        <slot name="bt-dev-name">No Title Slot Provided</slot>
+        <slot name="bt-addr">No Title Slot Provided</slot>
+        <!-- Gives the implementer the ability to override default styles -->
+        <h5 part="rando">blah blah</h5>
+    </div>
+`;
+class BluetoothDevContainer extends HTMLElement {
+    
     constructor(){
         super();
-    }
+        console.log("creating bt container")
+        // This is the "container/sandbox/canvas" for the custom element
+        const shadowRoot = this.attachShadow({mode: 'closed'});
 
-    connectedCallback() {
-        // DOM - Document Object Model
-        // access the shadow DOM
-        const shadow = this.shadowRoot;
+        // let div = document.createElement('div');
+        // div.textContent = 'BT DEV Container';
+        // shadowRoot.append(div);
 
-        // access an element inside the shadow DOM
-        const contentDiv = shadow.querySelector('.custom-element-content');
-
-        // ad an event listener or perform other interactions
-        contentDiv.addEventListener('click', () => {
-            console.log('Custom element clicked!');
-        });
+        let clone = template.content.cloneNode(true);
+        shadowRoot.appendChild(clone);
     }
 
 }
 
-customElements.define('bluetoothdevbox', BluetoothDevBox);
+customElements.define('bt-dev-container', BluetoothDevContainer);
