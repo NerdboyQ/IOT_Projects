774d01eae26ae390e245b4a57957838a52d4f3bf
NerdboyQ <princtonb@gmail.com>
2024-03-23 11:29:04 -0700

    added main js file for UI functions

---
+++ BT_Lights/src/webapp/flask/bt_iot_webapp/static/js/controls.js
@@ -0,0 +1,42 @@
+$(document).ready(main);
+
+function main(){
+    const bt_scan_btn = document.getElementById("bt-scan-btn");
+    var bt_device_tbody = document.getElementById("bt-dev-tbody");
+    bt_scan_btn.addEventListener('click', run_bt_scan);
+
+    function run_bt_scan(){
+        console.log("clicked");
+        console.log(bt_scan_btn.disabled);
+        bt_device_tbody.innerHTML = '';
+
+        $.getJSON(
+            "/scan-for-bt-devices",
+            function(data){
+                console.log(data);
+                for (var key in data){
+                    console.log(key);
+                    for (var dev of data[key]){
+                        console.log(dev);
+                        var tr = document.createElement("tr");
+                        var bt = document.createElement("bt-dev-container");
+                        var h4 = document.createElement("h4");
+                        h4.setAttribute('slot', 'bt-dev-name');
+                        h4.textContent = dev['name'];
+                        var h5 = document.createElement("h5");
+                        h5.setAttribute('slot', 'bt-addr');
+                        h5.textContent = dev['bt_addr'];
+                        bt.appendChild(h4);
+                        bt.appendChild(h5);
+                        tr.appendChild(bt);
+
+                        bt_device_tbody.appendChild(tr);
+                    }
+                }
+            }
+        );
+    }
+
+}
+
+
